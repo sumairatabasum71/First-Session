ba5ccd90832a01074ec7261c88d712ed83726558
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-02-18 14:12:06 +0500

    these are 2 files my 2nd question assighment

---
+++ assighment.js
@@ -0,0 +1,4 @@
+//  personal message: store a person's name in a variable,and print a message to
+//  that person.your message show such as,hello Eric,would you like to learn some python today?
+var pername = "Sumaira";
+console.log("\"Hello ".concat(pername, ", would you like to learn some Typescript today?\""));
---
+++ assighment.ts
@@ -0,0 +1,5 @@
+//  personal message: store a person's name in a variable,and print a message to
+//  that person.your message show such as,hello Eric,would you like to learn some python today?
+
+let pername:string="Sumaira";
+console.log(`"Hello ${pername}, would you like to learn some Typescript today?"`);
